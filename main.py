8bd3e4a523610a7f37c0ecb7a0274fdc91801606
monirul8888 <monirul429588@gmail.com>
2026-04-06 21:51:37 +0600

    Added Database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 
 
@@ -9,7 +11,12 @@ app = FastAPI()
 
 @app.get("/")
 def view():
-    return {"message" : "Welcome To FastAPI"}
+
+    cursor.execute(""" SELECT * FROM STUDENT """)
+    data = cursor.fetchall()
+    return {"message" : "Welcome To FastAPI",
+            "Data": data}
+
 
 @app.get("/about")
 def about():
@@ -27,4 +34,23 @@ def createStudent(st: Student):
     return {"data" : st}
 
 
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host = "localhost",
+            database = "university",
+            user = "postgres",
+            password = "1234",
+            cursor_factory = RealDictCursor
+        )
+
+        cursor = conn.cursor()
+        print("Successfully Connected To The Database")
+        break
+    except Exception as error:
+        print("Database Connection Failed")
+        print("Error : ", error)
+        time.sleep(2)
 
